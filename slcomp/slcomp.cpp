f587f222e91b4da7ba99b0018e5e6be8f9f460cb
Paul Gregory <pgregory@users.sourceforge.net>
2001-05-08 17:04:43 +0000

    Added default header, and reordered includes to get rid of warnings.

--- slcomp/slcomp.cpp
+++ slcomp/slcomp.cpp
@@ -1,5 +1,30 @@
-// comptest.cpp : Defines the entry point for the console application.
+// Aqsis
+// Copyright © 1997 - 2001, Paul C. Gregory
 //
+// Contact: pgregory@aqsis.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+/** \file
+		\brief Implements the entrypoint for the shader compiler.
+		\author Paul C. Gregory (pgregory@aqsis.com)
+*/
+
+#include	"aqsis.h"
+
 #ifdef	AQSIS_SYSTEM_WIN32
 	#include	<windows.h>
 #endif // AQSIS_SYSTEM_WIN32
@@ -7,7 +32,6 @@
 #include	<iostream>
 #include	<fstream>
 
-#include	"aqsis.h"
 #include	"parsenode.h"
 #include	"ri.h"
 #include	"Parser.h"
