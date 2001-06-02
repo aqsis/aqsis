b1feb54b07cfd9f6d26cfe227d81e8f633f4a1df
Paul Gregory <pgregory@users.sourceforge.net>
2001-06-02 09:18:26 +0000

    Moved into separate libraries, libslparse and libcodegenvm

--- slcomp/error.h
+++
@@ -1,7 +0,0 @@
-/* Simple SL include file with errors in it */
-
-#define snoise(x)    (2*noise(x) - 1)
-#define PULSE(a,b,x) (step((a),(x)) - step((b),(x)))
-#define boxstep(a,b,x) clamp(((x)-(a))/((b)-(a)),0,1)
-
-
