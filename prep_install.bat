04f61cafa0a6efa082b7f1929e3e3f7cd8c2a9e1
Paul Gregory <pgregory@users.sourceforge.net>
2001-10-24 22:25:30 +0000

    Updates prior to 0.5.3000 windows release.

--- prep_install.bat
+++ prep_install.bat
@@ -1,20 +1,22 @@
 @echo off
-mkdir ..\binary-archive
-mkdir ..\binary-archive\shaders
-mkdir ..\binary-archive\docs
-mkdir ..\binary-archive\examples
+if not exist ..\binary-archive mkdir ..\binary-archive
+if not exist ..\binary-archive\shaders mkdir ..\binary-archive\shaders
+if not exist ..\binary-archive\docs mkdir ..\binary-archive\docs
+if not exist ..\binary-archive\examples mkdir ..\binary-archive\examples
 
+erase /f /s /q ..\binary-archive\*.*
 
 cd ..\binary-archive
 
 copy ..\library\release\aqsis.exe .
 copy ..\library\release\filebuffer.exe .
-copy ..\library\release\framebuffer.exe .
+copy ..\library\release\framebuffer_glut.exe .
+copy ..\library\release\framebuffer_glut_z.exe .
 copy ..\library\release\shadowmap.exe .
 copy ..\library\release\aqsl.exe .
 copy ..\library\release\aqslcomp.exe .
 copy ..\library\release\slpp.exe .
-copy ..\library\release\texer.exe .
+copy ..\library\release\teqser.exe .
 copy ..\library\release\ddmsock.ini .
 
 copy \Libraries\tiff-v3.5.5\libtiff\libtiff.dll .
@@ -37,4 +39,5 @@ cd ..\examples
 copy ..\..\ribfiles\*.rib .
 
 cd ..\..\
+erase /f /q Aqsis_Win32.zip
 wzzip -a -r -p Aqsis_Win32.zip binary-archive\*.* 
