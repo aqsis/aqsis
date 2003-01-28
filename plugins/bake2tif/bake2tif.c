42f9bea5992409f336781f4f7d6d9d1da9ab622f
Paul Gregory <pgregory@users.sourceforge.net>
2003-01-28 00:02:01 +0000

    Implement linear interpolation in pixels where no data exists in the bake file.         - Michel Joron

--- plugins/bake2tif/bake2tif.c
+++ plugins/bake2tif/bake2tif.c
@@ -1,5 +1,5 @@
 // Aqsis
-// Copyright © 1997 - 2001, Paul C. Gregory
+// Copyright ) 1997 - 2001, Paul C. Gregory
 //
 // Contact: pgregory@aqsis.com
 //
@@ -7,12 +7,12 @@
 // modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation; either
 // version 2 of the License, or (at your option) any later version.
-// 
+//
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -25,13 +25,13 @@
 
 
 // The conversion process assumes a minimal conversion
-// the function to do convertion must be name 
-// as the name of the dll or .so on Unix 
+// the function to do convertion must be name
+// as the name of the dll or .so on Unix
 // The conversion must create a valid .tif file and return
 // the filename of the new file created.
 // Eg. this function bake2tif() created a .tif file at the same place
 // as the .bake filename provided.
-// IN ORDER TO CONVERSION OCCURRED the dll/dso must be present in 
+// IN ORDER TO CONVERSION OCCURRED the dll/dso must be present in
 // procedures directory under <AQSIS_BASE_PATH>
 
 #include <stdio.h>
@@ -44,11 +44,11 @@
 #define	_qShareName	bake2tif
 #include	"share.h"
 
-static char *bake_open(FILE *bakefile, char *tiffname);
+static char *bake_open( FILE *bakefile, char *tiffname );
 
-static char tiffname[1024];
+static char tiffname[ 1024 ];
 
-/* Main function to convert any bake to tif format 
+/* Main function to convert any bake to tif format
  * It used the standard standard definition of bake file defined in bake() from
  * Siggraph 2001.
  *
@@ -59,119 +59,157 @@ static char tiffname[1024];
  * need more and agressive filtering (between subsamples s/t)
  */
 #define SIZE 64
+#define REMOVEDARK 1
 
 static int size = SIZE;
 
-_qShareM char *bake2tif(char *in)
+_qShareM char *bake2tif( char *in )
 {
-FILE *bakefile;
-char *result =NULL;
+	FILE * bakefile;
+	char *result = NULL;
 
-    
+
 	result = ( char * ) getenv( "BAKE" );
 
 	if ( result && ( result[ 0 ] >= '0' ) && ( result[ 0 ] <= '9' ) )
 		size = atoi( result );
 
-	strcpy(tiffname, in);
-	if ((result = strstr(tiffname, ".bake")) != 0) strcpy(result, ".tif");
-	if (!result) 
+	strcpy( tiffname, in );
+	if ( ( result = strstr( tiffname, ".bake" ) ) != 0 ) strcpy( result, ".tif" );
+	if ( !result )
 	{
-		if ((result = strstr(tiffname, ".bake")) != 0) strcpy(result, ".tif");
+		if ( ( result = strstr( tiffname, ".bake" ) ) != 0 ) strcpy( result, ".tif" );
 	}
-	if (!result) return result;
+	if ( !result ) return result;
 
-	bakefile = fopen(in,"rb");
-	result = bake_open(bakefile, tiffname);
-	fclose(bakefile);
+	bakefile = fopen( in, "rb" );
+	result = bake_open( bakefile, tiffname );
+	fclose( bakefile );
 
 
 	return result;
 }
 
-
 /* PRIVATE FUNCTIONS */
+/*
+ * linear interpretation between an sample at the left side and ride side of black pixel.
+ */
+
+static int lerp(int maxy, int miny, int maxx, int minx, int x)
+{
+int result = minx;
+float ratio = 1.0;
+
+    if (x == maxx) return maxy;
+    if (x == minx) return miny;
+	if (maxx == minx) return miny;
+	else 
+		ratio = 1.0 - (float) (maxx - x) / (float) (maxx - minx);
+	result = miny + (int) (ratio * (maxy - miny));
+
+	
+    return result;
+}
+
 /*
  * bake_open(): extract the colourmap of BAKE; compute the 
  *   pixels in RGB format and finally save to a tiff file
  */
-static char *bake_open(FILE *bakefile, char *tiffname) 
+static char *bake_open( FILE *bakefile, char *tiffname )
 {
-   unsigned short  h, w;
-   float s,t,r1,g1,b1;
-   unsigned char *pixels;
-   char buffer[200];
-	int i, j, n;
+	unsigned short h, w;
+	float s, t, r1, g1, b1;
+	unsigned char *pixels;
+	char buffer[ 200 ];
+	int i, j, n, o;
 	int x, y;
-	unsigned char r, g, b, or, og, ob;
 
 	h = w = size;
 	pixels = ( unsigned char * ) calloc( 3, size * size );
 
-   while ( fgets(buffer, 200, bakefile) != NULL) 
-   {
+	while ( fgets( buffer, 200, bakefile ) != NULL )
+	{
 
-   if (5 ==sscanf(buffer, "%f %f %f %f %f", &s, &t, &r1, &g1, &b1));
-   else
-   {
-      	sscanf(buffer, "%f %f %f", &s, &t, &r1 );
-        g1 = b1 = r1;
-   }
+		if ( 5 == sscanf( buffer, "%f %f %f %f %f", &s, &t, &r1, &g1, &b1 ) );
+		else
+		{
+			sscanf( buffer, "%f %f %f", &s, &t, &r1 );
+			g1 = b1 = r1;
+		}
 
 		// When we have some collision ? What should be nice
 		// to accumulate the RGB values instead ?
 		x = ( s * ( size - 1 ) );
 		y = ( t * ( size - 1 ) );
 		n = ( y * size ) + x;
-     n *= 3;
+		n *= 3;
 
-#ifdef ACCUM
-		if ( ( pixels[ n ] == pixels[ n + 1 ] ) &&
-		        ( pixels[ n + 1 ] == pixels[ n + 2 ] ) &&
-		        ( pixels[ n + 2 ] == 0 ) )
+		pixels [ n ] = r1 * 255;
+		pixels [ n + 1 ] = g1 * 255;
+		pixels [ n + 2 ] = b1 * 255;
+
+	}
+
+#ifdef REMOVEDARK
+
+	for (i=0; i < size; i++)
+	{
+		for(j =0; j < size* 3; j+=3)
 		{
-			pixels [ n ] = r1 * 255;
-			pixels [ n + 1 ] = g1 * 255;
-			pixels [ n + 2 ] = b1 * 255;
-   }
-		else
-		{
-			pixels [ n ] = ( ( pixels[ n ] / 255.0f + r1 ) / 2.0 ) * 255;
-			pixels [ n + 1 ] = ( ( pixels[ n + 1 ] / 255.0f + g1 ) / 2.0 ) * 255;
-			pixels [ n + 2 ] = ( ( pixels[ n + 2 ] / 255.0f + b1 ) / 2.0 ) * 255;
+			n = (i * size * 3) + j;
+			if ( ( pixels[ n ] == pixels[ n + 1 ] ) &&
+			( pixels[ n + 1 ] == pixels[ n + 2 ] ) &&
+			( pixels[ n + 2 ] == 0 ) )
+			{
+				int m =  n-3;
+				while (m > 0) {
+					if (!( ( pixels[ m ] == pixels[ m + 1 ] ) &&
+					( pixels[ m + 1 ] == pixels[ m + 2 ] ) &&
+					( pixels[ m + 2 ] == 0 ) ))
+					break;
+					m-=3;
+				}
+				o = n + 3;
+				while (o < size * size * 3) {
+					if (!( ( pixels[ o ] == pixels[ o + 1 ] ) &&
+					( pixels[ o + 1 ] == pixels[ o + 2 ] ) &&
+					( pixels[ o + 2 ] == 0 ) ))
+					break;
+					o+=3;
+				}
+				pixels[ n ] = lerp(pixels[o], pixels[m], o, m, n);
+				pixels[ n +1] = lerp(pixels[o+1], pixels[m+1], o, m, n);
+				pixels[ n +2] = lerp(pixels[o+2], pixels[m+2], o, m, n);
+			}
 		}
-#else
-			pixels [ n ] = r1 * 255;
-			pixels [ n + 1 ] = g1 * 255;
-			pixels [ n + 2 ] = b1 * 255;
-#endif
-  }
+	}
 
+#endif
 	// Should we do some filterings prior to save to tif file ?
 	// I will used the general scheme of mipmap from texturemap.cpp for now
 
-   /* convert each scan line */ 
-   save_tiff(tiffname, pixels, w, h, 3,"bake2tif");
+	/* convert each scan line */
+	save_tiff( tiffname, pixels, w, h, 3, "bake2tif" );
 
-   free(pixels); 
-   return tiffname;
-} 
+	free( pixels );
+	return tiffname;
+}
 
 #ifdef MAIN
-int main(int argc, char *argv[])
+int main( int argc, char *argv[] )
 {
-char *result;
+	char * result;
 
 	if ( argc != 2 )
 	{
-      fprintf(stderr, "Usage %s: %s some.bake", argv[0], argv[1]);
-      exit(2);
-   }
-   result = bake2tif(argv[1]);
+		fprintf( stderr, "Usage %s: %s some.bake", argv[ 0 ], argv[ 1 ] );
+		exit( 2 );
+	}
+	result = bake2tif( argv[ 1 ] );
 	if ( result )
 	{
-      puts(result);
-   }
-   return 1;
+		puts( result );
+	}
+	return 1;
 }
 #endif
