5eb6e9daf969dd36c291ea97a98260a0e9d27f0a
Paul Gregory <pgregory@users.sourceforge.net>
2003-03-20 13:45:38 +0000

    Added system specific implementation file for CqFile functions specific to Win32/intel

---
+++ libaqsistypes/win32/intel/file_system.cpp
@@ -0,0 +1,138 @@
+// Aqsis
+// Copyright © 1997 - 2001, Paul C. Gregory
+//
+// Contact: pgregory@aqsis.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+/** \file
+		\brief Implements the system specific parts of the CqFile class for handling files.
+		\author Paul C. Gregory (pgregory@aqsis.com)
+*/
+
+#include	<fstream>
+
+#include	<string.h>
+#include	<ctype.h>
+
+#include	"aqsis.h"
+
+#include	"file.h"
+
+START_NAMESPACE( Aqsis )
+
+//---------------------------------------------------------------------
+/** Get the searchpath for a the specified asset type. The way in which the
+ *  searchpath is specified is OS specific. Under Windows, environment variables
+ *  are used.
+ *
+ *  \param strAsset The name of the asset whose searchpath is to be returned, i.e. "shaders".
+ *  \return A string containing the ":" separated list of searchpaths.
+ */
+
+CqString CqFile::GetSystemSetting(const CqString& strAsset)
+{
+	char* env;
+	char* base_path;
+	CqString result("");
+	
+	if ( ( env = getenv( "AQSIS_BASE_PATH" ) ) != NULL )
+		base_path = env;
+
+
+	if( strAsset.compare( "base" ) == 0 )
+	{
+		result = base_path;
+	}
+	else if( strAsset.compare( "config" ) == 0 )
+	{
+		if ( ( env = getenv( "AQSIS_CONFIG" ) ) != NULL )
+			result = env;
+		else
+		{
+			result = base_path;
+			result.append( "/.aqsisrc" );
+		}
+
+		std::ifstream cfgfile( result.c_str() );
+		if ( !cfgfile.is_open() )
+			if ( ( env = getenv( "HOME" ) ) != NULL )
+			{
+				result = env;
+				result.append( "/.aqsisrc" );
+				std::ifstream cfgfile( result.c_str() );
+				if ( !cfgfile.is_open() )
+				{
+					result = ".aqsisrc";
+				}
+			}
+	}
+	else if( strAsset.compare( "shaders" ) == 0 )
+	{
+		if ( ( env = getenv( "AQSIS_SHADERS_PATH" ) ) != 0 )
+			result = env;
+		else
+		{
+			result = base_path;
+			result.append( "/shaders" );
+		}
+	}
+	else if( strAsset.compare( "archives" ) == 0 )
+	{
+		if ( ( env = getenv( "AQSIS_ARCHIVES_PATH" ) ) != 0 )
+			result = env;
+		else
+		{
+			result = base_path;
+			result.append( "/archives" );
+		}
+	}
+	else if( strAsset.compare( "textures" ) == 0 )
+	{
+		if ( ( env = getenv( "AQSIS_TEXTURES_PATH" ) ) != 0 )
+			result = env;
+		else
+		{
+			result = base_path;
+			result.append( "/textures" );
+		}
+	}
+	else if( strAsset.compare( "displays" ) == 0 )
+	{
+		if ( ( env = getenv( "AQSIS_DISPLAYS_PATH" ) ) != 0 )
+			result = env;
+		else
+		{
+			result = base_path;
+			result.append( "/displays" );
+		}
+	}
+	else if( strAsset.compare( "dsolibs" ) == 0 )
+	{
+		if ( ( env = getenv( "AQSIS_DSO_LIBS" ) ) != 0 )
+			result = env;
+		else
+		{
+			result = base_path;
+			result.append( "/dso" );
+		}
+	}
+
+	return( result );
+}
+
+END_NAMESPACE( Aqsis )
+//---------------------------------------------------------------------
