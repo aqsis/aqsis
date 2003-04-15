9558c9c19f0b4ecf597b9dabb5c7105e07013c57
Tristan Colgate <minty@users.sourceforge.net>
2003-04-15 22:20:24 +0000

    Revert previous change and add a notice about dependency issue.

--- libshadervm/shadervariable.cpp
+++ libshadervm/shadervariable.cpp
@@ -30,24 +30,28 @@
 #include	"shadervariable.h"
 #include	"shadervm.h"
 #include	"irenderer.h"
-#include	"renderer.h"
+//#include	"renderer.h"
 
 START_NAMESPACE( Aqsis )
 
 
+ /*
+  * FIXME: These are commented out as they introduce a dependency on libaqsis
+  * via the extern pCurrRenderer in renderer.h
+  */
 CqShaderVariable::CqShaderVariable() : m_fParameter( TqFalse )
 {
-	QGetRenderContext() ->Stats().IncVariablesAllocated();
+//	QGetRenderContext() ->Stats().IncVariablesAllocated();
 }
 
 CqShaderVariable::CqShaderVariable( const char* strName, TqBool fParameter ) : m_strName( strName ), m_fParameter( fParameter )
 {
-	QGetRenderContext() ->Stats().IncVariablesAllocated();
+//	QGetRenderContext() ->Stats().IncVariablesAllocated();
 }
 
 CqShaderVariable::~CqShaderVariable()
 {
-	QGetRenderContext() ->Stats().IncVariablesDeallocated();
+//	QGetRenderContext() ->Stats().IncVariablesDeallocated();
 }
 
 
