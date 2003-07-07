195e72c07683c2f4ca16598fb70789e3535e3d62
Paul Gregory <pgregory@users.sourceforge.net>
2003-07-07 00:08:01 +0000

    Initial implementation on Windows, doesn't work yet.

--- render/procedural.cpp
+++ render/procedural.cpp
@@ -266,6 +266,7 @@ extern "C" RtVoid	RiProcDelayedReadArchive( RtPointer data, RtFloat detail )
 // process.
 
 #ifndef	AQSIS_SYSTEM_WIN32
+
 class CqRiProceduralRunProgram
 {
 	public:
@@ -276,12 +277,9 @@ class CqRiProceduralRunProgram
 };
 
 static std::map<std::string, CqRiProceduralRunProgram*> ActiveProcRP;
-#endif
 
 extern "C" RtVoid	RiProcRunProgram( RtPointer data, RtFloat detail )
 {
-#ifdef AQSIS_SYSTEM_WIN32
-#else
 	std::map<std::string, CqRiProceduralRunProgram*>::iterator it;
 
 	it = ActiveProcRP.find(CqString( ((char**)data)[0] )) ;
@@ -378,9 +376,238 @@ extern "C" RtVoid	RiProcRunProgram( RtPointer data, RtFloat detail )
 
 	delete( decoder );
 
-#endif
 	return;
 }
+
+#else
+
+#include <windows.h>
+#include <fcntl.h>
+
+class CqRiProceduralRunProgram
+{
+	public:
+	HANDLE hChildStdinWrDup;
+	HANDLE hChildStdoutRdDup;
+};
+
+static std::map<std::string, CqRiProceduralRunProgram*> ActiveProcRP;
+
+BOOL CreateChildProcess( char* command) 
+{ 
+	PROCESS_INFORMATION piProcInfo; 
+	STARTUPINFO siStartInfo;
+	BOOL bFuncRetn = FALSE; 
+ 
+// Set up members of the PROCESS_INFORMATION structure. 
+ 
+	ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
+ 
+// Set up members of the STARTUPINFO structure. 
+ 
+	ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
+	siStartInfo.cb = sizeof(STARTUPINFO); 
+ 
+// Create the child process. 
+    
+	bFuncRetn = CreateProcess(NULL, 
+								command,       // command line 
+								NULL,          // process security attributes 
+								NULL,          // primary thread security attributes 
+								TRUE,          // handles are inherited 
+								0,             // creation flags 
+								NULL,          // use parent's environment 
+								NULL,          // use parent's current directory 
+								&siStartInfo,  // STARTUPINFO pointer 
+								&piProcInfo);  // receives PROCESS_INFORMATION 
+   
+	if (bFuncRetn == 0) 
+	{
+		QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - CreateProcess failed" );
+		return 0;
+	} 
+	else 
+	{
+		CloseHandle(piProcInfo.hProcess);
+		CloseHandle(piProcInfo.hThread);
+		return bFuncRetn;
+	}
+}
+
+
+extern "C" RtVoid	RiProcRunProgram( RtPointer data, RtFloat detail )
+{
+	HANDLE hChildStdinRd, hChildStdinWr, hChildStdinWrDup, 
+		hChildStdoutRd, hChildStdoutWr, hChildStdoutRdDup, 
+		hSaveStdin, hSaveStdout; 
+
+	std::map<std::string, CqRiProceduralRunProgram*>::iterator it;
+
+	it = ActiveProcRP.find(CqString( ((char**)data)[0] )) ;
+	if( it == ActiveProcRP.end() )
+	{
+		// We don't have an active RunProgram for the specifed
+		// program. We need to try and fork a new one
+		CqRiProceduralRunProgram *run_proc = new CqRiProceduralRunProgram;
+
+		ActiveProcRP[std::string(((char**)data)[0])] = run_proc;
+		it = ActiveProcRP.find(std::string(((char**)data)[0])); 
+
+		SECURITY_ATTRIBUTES saAttr; 
+		BOOL fSuccess; 
+
+	// Set the bInheritHandle flag so pipe handles are inherited. 
+
+		saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
+		saAttr.bInheritHandle = TRUE; 
+		saAttr.lpSecurityDescriptor = NULL; 
+
+	   // The steps for redirecting child process's STDOUT: 
+	   //     1. Save current STDOUT, to be restored later. 
+	   //     2. Create anonymous pipe to be STDOUT for child process. 
+	   //     3. Set STDOUT of the parent process to be write handle to 
+	   //        the pipe, so it is inherited by the child process. 
+	   //     4. Create a noninheritable duplicate of the read handle and
+	   //        close the inheritable read handle. 
+
+	// Save the handle to the current STDOUT. 
+
+		hSaveStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+
+	// Create a pipe for the child process's STDOUT. 
+
+		if (! CreatePipe(&hChildStdoutRd, &hChildStdoutWr, &saAttr, 0)) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - Stdout pipe creation failed" );
+			return;
+		}
+
+	// Set a write handle to the pipe to be STDOUT. 
+
+		if (! SetStdHandle(STD_OUTPUT_HANDLE, hChildStdoutWr)) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - Redirecting STDOUT failed" );
+			return;
+		}
+
+	// Create noninheritable read handle and close the inheritable read 
+	// handle. 
+
+		fSuccess = DuplicateHandle(GetCurrentProcess(), hChildStdoutRd,
+								   GetCurrentProcess(), &hChildStdoutRdDup , 0,
+								   FALSE,
+								   DUPLICATE_SAME_ACCESS);
+		if( !fSuccess )
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - DuplicateHandle failed" );
+			return;
+		}
+		CloseHandle(hChildStdoutRd);
+
+	// The steps for redirecting child process's STDIN: 
+	//     1.  Save current STDIN, to be restored later. 
+	//     2.  Create anonymous pipe to be STDIN for child process. 
+	//     3.  Set STDIN of the parent to be the read handle to the 
+	//         pipe, so it is inherited by the child process. 
+	//     4.  Create a noninheritable duplicate of the write handle, 
+	//         and close the inheritable write handle. 
+
+	// Save the handle to the current STDIN. 
+
+		hSaveStdin = GetStdHandle(STD_INPUT_HANDLE); 
+
+	// Create a pipe for the child process's STDIN. 
+
+		if (! CreatePipe(&hChildStdinRd, &hChildStdinWr, &saAttr, 0)) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - Stdin pipe creation failed" );
+			return;
+		} 
+
+	// Set a read handle to the pipe to be STDIN. 
+
+		if (! SetStdHandle(STD_INPUT_HANDLE, hChildStdinRd)) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - Redirecting Stdin failed" );
+			return; 
+		}
+
+	// Duplicate the write handle to the pipe so it is not inherited. 
+
+		fSuccess = DuplicateHandle(GetCurrentProcess(), hChildStdinWr, 
+								   GetCurrentProcess(), &hChildStdinWrDup, 0, 
+								   FALSE,                  // not inherited 
+								   DUPLICATE_SAME_ACCESS); 
+		if (! fSuccess) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - DuplicateHandle failed" );
+			return; 
+		}
+
+		CloseHandle(hChildStdinWr); 
+
+	// Now create the child process. 
+
+		// We should use the procedurals searchpath here
+		fSuccess = CreateChildProcess(((char**)data)[0]);
+		if (! fSuccess) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - Create process failed" );
+			return; 
+		}
+
+	// After process creation, restore the saved STDIN and STDOUT. 
+
+		if (! SetStdHandle(STD_INPUT_HANDLE, hSaveStdin)) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - Re-redirecting Stdin failed" );
+			return; 
+		}
+
+		if (! SetStdHandle(STD_OUTPUT_HANDLE, hSaveStdout)) 
+		{
+			QGetRenderContextI() ->Logger() ->error( "RiProcRunProgram - Re-redirecting Stdout failed" );
+			return; 
+		}
+		// Store the handles.
+		(it->second)->hChildStdinWrDup = hChildStdinWrDup;
+		(it->second)->hChildStdoutRdDup = hChildStdoutRdDup;
+	}
+
+	int fd_hChildStdinWrDup = _open_osfhandle((long)(it->second)->hChildStdinWrDup, 0);
+	FILE *fileout = _fdopen( fd_hChildStdinWrDup, "w");
+	// Write out detail and data to the process
+	fprintf( fileout, "%g %s\n", detail, ((char**)data)[1] );
+	fflush( fileout );
+	// should check for a SIGPIPE here incase the RunProgram died.
+	 
+	// This is not pretty, gzopen attempts to read a gzip header
+	//from the stream, if we try and do this before we have sent
+	//the request the we get deadlock, so we have to create the decoder
+	//after the request, we use a buffer size of one to prevent any 
+	//potential blocking.
+	CqRibBinaryDecoder *decoder;
+	int fd_hChildStdoutRdDup = _open_osfhandle((long)(it->second)->hChildStdoutRdDup, O_RDONLY);
+	FILE *filein = _fdopen( fd_hChildStdoutRdDup, "r" );
+	decoder = new CqRibBinaryDecoder( filein, 1);
+
+	// Parse the resulting block of RIB.
+	CqString strRealName( ((char**)data)[0] );
+	CqRIBParserState currstate = librib::GetParserState();
+
+	if( currstate.m_pParseCallbackInterface == NULL ) currstate.m_pParseCallbackInterface = new librib2ri::Engine;
+
+	librib::ParseOpenStream( decoder, strRealName.c_str(), *(currstate.m_pParseCallbackInterface), *(currstate.m_pParseErrorStream), (RtArchiveCallback) RI_NULL );
+
+	librib::SetParserState( currstate );
+
+	delete( decoder );
+
+	return;
+}
+
+
+#endif
 
 END_NAMESPACE( Aqsis )
 
