6917350edf9c3b9bcb7a084ad8bd3a750284119d
Paul Gregory <pgregory@users.sourceforge.net>
2003-07-23 17:46:20 +0000

    Update to provide more information and to sort by function time.

--- ftime.bat
+++ ftime.bat
@@ -1,11 +1,11 @@
 CD Library\Profile
-PREP /OM /FT /EXCALL /INC libaqsis.lib /INC libaqsistypes.lib /INC libshaderexecenv.lib /INC libshadervm.lib /INC libddmsock.lib /INC librib2.lib aqsis.exe libaqsis.dll
+PREP /OM /FT /EXC librib2.lib aqsis.exe libaqsis.dll
 if errorlevel == 1 goto done 
 PROFILE aqsis %1 %2 %3 %4 %5 %6 %7 %8 %9
 if errorlevel == 1 goto done 
 PREP /M aqsis
 if errorlevel == 1 goto done 
-PLIST /SC aqsis >aqsis.ftime.lst
+PLIST /ST aqsis >aqsis.ftime.lst
 MOVE aqsis.ftime.lst ..\..
 :done
 CD ..\..
