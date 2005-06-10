75d1ba95884442acec29dc2fa30f956defbeb1ad
Mattheus Chajdas <mw_matti@users.sourceforge.net>
2005-06-10 15:50:05 +0000

    removed inline comment in line 137

--- render/stats.h
+++ render/stats.h
@@ -134,7 +134,8 @@ public:
     /**
     	\param	value	This has to be a 32-bit integer!
      */
-    static unsigned int log2( int value /* Must be 32-bit integer!! */ )
+    /* Must be 32-bit integer!! */
+    static unsigned int log2( int value )
     {
         // As described in http://graphics.stanford.edu/~seander/bithacks.html#IntegerLog
         // This method should take roughly O(lg(N)) operations for a N-bit integer
