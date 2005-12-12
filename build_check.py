c33089bdb4f4cd78a6476c628b852e37c0995ca3
Paul Gregory <pgregory@users.sourceforge.net>
2005-12-12 14:25:34 +0000

    Add a check for FLTK

--- build_check.py
+++ build_check.py
@@ -1,19 +1,24 @@
-Import('env conf')
+Import('conf')
 
 # Check for the availability of various important files.
 
 # Check for the TIFF libraries and headers
-if not conf.CheckLibWithHeader(env.subst('$tiff_lib'), 'tiffio.h', 'c++'):
+if not conf.CheckLibWithHeader(conf.env.subst('$tiff_lib'), 'tiffio.h', 'c++'):
 	print 'Cannot find libtiff'
 	Exit(1)
 
 # Check for the JPEG libraries and headers
-#if not conf.CheckLibWithHeader(env.subst('$jpeg_lib'), ['stdio.h', 'jpeglib.h'], 'c++'):
+#if not conf.CheckLibWithHeader(conf.env.subst('$jpeg_lib'), ['stdio.h', 'jpeglib.h'], 'c++'):
 #	print 'Cannot find libjpeg'
 #	Exit(1)
 
 # Check for the zlib libraries and headers
-if not conf.CheckLibWithHeader(env.subst('$z_lib'), 'zlib.h', 'c++'):
+if not conf.CheckLibWithHeader(conf.env.subst('$z_lib'), 'zlib.h', 'c++'):
 	print 'Cannot find zlib'
 	Exit(1)
 
+# Check for the FLTK libraries and headers
+if not conf.CheckLibWithHeader(conf.env.subst('$fltk_lib'), ['stdio.h', 'FL/fl.h'], 'c++'):
+	print 'Cannot find FLTK'
+	conf.env.Replace(display_cppdefines = ['AQSIS_NO_FLTK'])
+	conf.env.Replace(fltk_lib = '')
