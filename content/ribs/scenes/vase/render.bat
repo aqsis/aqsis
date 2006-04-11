85cd370b6862c6f2b20fbc6e7828d4008d6fd290
Leon Atkinson <ltatkinson@users.sourceforge.net>
2006-04-11 22:58:25 +0000

    Examples updated to support BAT file rendering.

--- content/ribs/scenes/vase/render.bat
+++ content/ribs/scenes/vase/render.bat
@@ -1,13 +1,29 @@
 @ECHO OFF
 
+REM ***Compile shaders***
 
-REM ***Compile additional shaders***
-
-CLS
+ECHO === Compiling Shader(s) ===
+ECHO.
 aqsl.exe "../../../shaders/displacement/dented.sl" "../../../shaders/light/shadowspot.sl"
+IF NOT ERRORLEVEL 0 GOTO error
 
 
-REM ***Render example file***
+REM ***Render files***
 
-CLS
+ECHO.
+ECHO.
+ECHO === Rendering File(s) ===
+ECHO.
 aqsis.exe -progress "vase.rib"
+IF ERRORLEVEL 0 GOTO end
+
+
+REM ***Error reporting***
+
+:error
+ECHO.
+ECHO.
+ECHO An error occured, please read messages !!!
+PAUSE
+EXIT
+:end
