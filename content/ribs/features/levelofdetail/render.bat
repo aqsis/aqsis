409c2322a38b1c127105fbffbd18402f416e54a4
Leon Atkinson <ltatkinson@users.sourceforge.net>
2006-11-03 14:36:21 +0000

    Content/example update.

---
+++ content/ribs/features/levelofdetail/render.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+REM ***Render files***
+
+ECHO.
+ECHO.
+ECHO === Rendering File(s) ===
+ECHO.
+aqsis.exe -progress "detail.rib"
+IF ERRORLEVEL 0 GOTO end
+
+
+REM ***Error reporting***
+
+:error
+ECHO.
+ECHO.
+ECHO An error occured, please read messages !!!
+PAUSE
+EXIT
+:end
