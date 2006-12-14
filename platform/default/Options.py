1accb79b347d857055cfda07a77e284284b64325
Paul Gregory <pgregory@users.sourceforge.net>
2006-12-14 10:57:21 +0000

    Add a new option to allow the disabling of rpath in the Posix build, required for RPM building.

--- platform/default/Options.py
+++ platform/default/Options.py
@@ -8,4 +8,5 @@ def PlatformOptions(opts, tempenv):
 	opts.Add('libdir', 'Directory into which libraries will be installed', '$install_prefix/lib')
 	opts.Add('libexecdir', 'Directory into which executable subprograms will be installed', '$libdir/aqsis')
 	opts.Add('destdir', 'Temporary directory to prepend to all install paths (packagers only)', '')
+	opts.Add(BoolOption('no_rpath', 'Do not set the "rpath" in the executable/shared libraries', '0'))
 
