a0f71a2047018f3c0a68abf61124b6b0215efc02
Mattheus Chajdas <anteru@users.sourceforge.net>
2007-04-04 11:37:37 +0000

    * renamed Off to off to be in sync with the rest of SCons

--- platform/win32/Options.py
+++ platform/win32/Options.py
@@ -3,7 +3,7 @@ from SCons.Options import EnumOption, BoolOption
 
 def PlatformOptions(opts, tempenv):
 	opts.Add(EnumOption('MSVS_VERSION', 'MS Visual C++ Version', '6.0', allowed_values=('6.0', '7.1', '8.0')))
-	opts.Add(EnumOption('SSE', 'Enable SSE support', 'Off', allowed_values=('Off', 'SSE1', 'SSE2')))
+	opts.Add(EnumOption('SSE', 'Enable SSE support', 'off', allowed_values=('off', 'SSE1', 'SSE2')))
 	opts.Add(BoolOption('FP_Fast', 'Enable fast FP calculations at the expense of precision', 0))
 	opts.Add(BoolOption('mingw', 'Use MinGW compiler', 0))
 	opts.Add('bison', 'Point to the bison executable', tempenv.File('#/../win32libs/GnuWin32/bin/bison'))
