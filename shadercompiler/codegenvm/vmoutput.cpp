bb53686358a3c7acac8932f85f04674d42f105de
Zachary Carter <waynezacmp@users.sourceforge.net>
2007-05-19 08:33:39 +0000

    fix an error in vmdatagather.cpp and in vmoutput.cpp that were causing the debug build to fail

--- shadercompiler/codegenvm/vmoutput.cpp
+++ shadercompiler/codegenvm/vmoutput.cpp
@@ -640,7 +640,7 @@ void CqCodeGenOutput::Visit( IqParseNodeGatherConstruct& IC )
 	TqInt iLabelD = m_gcLabels++;
 
 	IqParseNode* pArgs = pNode->pChild();
-	assert( pArg != 0 );
+	assert( pArgs != 0 );
 	IqParseNode* pHitStmt = pArgs->pNextSibling();
 	assert( pHitStmt != 0 );
 	IqParseNode* pNoHitStmt = pHitStmt->pNextSibling();
