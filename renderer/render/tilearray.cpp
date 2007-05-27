a6625a201e7a63f64285eff517a34701e0cbf64d
Chris Foster <c42f@users.sourceforge.net>
2007-05-27 18:43:54 +0000

    * Add GPL notice to top of implementation files.

--- renderer/render/tilearray.cpp
+++ renderer/render/tilearray.cpp
@@ -1,3 +1,30 @@
+// Aqsis
+// Copyright © 1997 - 2001, Paul C. Gregory
+//
+// Contact: pgregory@aqsis.org
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/**
+ * \file tilearray.cpp
+ *
+ * \brief Implementation for classes dealing with tiled arrays.
+ *
+ * \author Chris Foster  chris42f _at_ gmail.com
+ */
+
 #include "tilearray.h"
 
 namespace Aqsis
