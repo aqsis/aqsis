fddb2a2cf7234d06673346238e7f16b62d541c25
Michel Joron <joron@users.sourceforge.net>
2007-07-05 13:34:31 +0000

    [Bug 1740063] Simplify the normal generation for points.cpp since the normal could not be assigned and assuming the occlusion for any points is good.

--- renderer/render/points.cpp
+++ renderer/render/points.cpp
@@ -188,12 +188,8 @@ CqMicroPolyGridBase* CqPoints::Dice()
 		CqVector3D	N(0,0,-1);
 		//N = QGetRenderContext() ->matSpaceToSpace( "camera", "object", NULL, pGrid->pTransform() ) * N;
 		TqUint u;
-		bool CSO = pTransform()->GetHandedness(pTransform()->Time(0));
-		bool O = pAttributes() ->GetIntegerAttribute( "System", "Orientation" ) [ 0 ] != 0;
-		bool revert = ( (O && CSO) || (!O && !CSO) );
 		for ( u = 0; u < nVertices(); u++ )
 		{
-			N = ( revert ) ? N : -N;
 			pGrid->pVar(EnvVars_Ng)->SetNormal( N, u );
 		}
 		pGrid->SetbGeometricNormals( true );
