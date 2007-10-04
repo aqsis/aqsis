8d8a4c55a5b2f32f0502e4159788c2ab4596f95c
Chris Foster <c42f@users.sourceforge.net>
2007-10-04 11:17:19 +0000

    * Move channel list class from piqsl into aqsistex; refactor slightly for the   new use.

---
+++ texturing/texlib/channellist.cpp
@@ -0,0 +1,101 @@
+// Aqsis
+// Copyright (C) 1997 - 2007, Paul C. Gregory
+//
+// Contact: pgregory@aqsis.org
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/** \file
+ *
+ * \brief Implementation of class encapsulating information about image channels
+ *
+ * \author Chris Foster
+ */
+
+#include "channellist.h"
+
+namespace Aqsis {
+
+//------------------------------------------------------------------------------
+// CqChannelList implementation
+CqChannelList CqChannelList::displayChannels()
+{
+    CqChannelList displayChannels;
+    displayChannels.addChannel(SqChannelInfo("r", Channel_Unsigned8));
+    displayChannels.addChannel(SqChannelInfo("g", Channel_Unsigned8));
+    displayChannels.addChannel(SqChannelInfo("b", Channel_Unsigned8));
+	return displayChannels;
+}
+
+void CqChannelList::addChannel(const SqChannelInfo& newChan)
+{
+	m_channels.push_back(newChan);
+	m_offsets.push_back(m_bytesPerPixel);
+	m_bytesPerPixel += newChan.bytesPerPixel();
+}
+
+void CqChannelList::reorderChannels()
+{
+	// If there are "r", "g", "b" and "a" channels, ensure they
+	// are in the expected order.
+	const char* elements[] = { "r", "g", "b", "a" };
+	TqInt numElements = sizeof(elements) / sizeof(elements[0]);
+	for(int elementIndex = 0; elementIndex < numElements; ++elementIndex)
+	{
+		for(TqListType::iterator channel = m_channels.begin(); channel != m_channels.end(); ++channel)
+		{
+			// If this entry in the channel list matches one in the expected list, 
+			// move it to the right point in the channel list.
+			if(channel->name == elements[elementIndex])
+			{
+				const SqChannelInfo temp = m_channels[elementIndex];
+				m_channels[elementIndex] = *channel;
+				*channel = temp;
+				break;
+			}
+		}
+	}
+	recomputeByteOffsets();
+}
+
+TqInt CqChannelList::findChannelIndexImpl(const std::string& name) const
+{
+	TqInt index = 0;
+	TqListType::const_iterator ichan = m_channels.begin();
+	while(ichan != m_channels.end() && ichan->name != name)
+	{
+		++ichan;
+		++index;
+	}
+	if(ichan == m_channels.end())
+		return -1;
+	return index;
+}
+
+void CqChannelList::recomputeByteOffsets()
+{
+	m_offsets.clear();
+	TqInt offset = 0;
+	for(TqListType::const_iterator chInfo = m_channels.begin();
+			chInfo != m_channels.end(); ++chInfo)
+	{
+		m_offsets.push_back(offset);
+		offset += chInfo->bytesPerPixel();
+	}
+	m_bytesPerPixel = offset;
+}
+
+
+} // namespace Aqsis
---
+++ texturing/texlib/channellist.h
@@ -0,0 +1,184 @@
+// Aqsis
+// Copyright (C) 1997 - 2007, Paul C. Gregory
+//
+// Contact: pgregory@aqsis.org
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/** \file
+ *
+ * \brief Declare class encapsulating information about image channels.
+ *
+ * \author Chris Foster
+ */
+
+#ifndef CHANNELLIST_H_INCLUDED
+#define CHANNELLIST_H_INCLUDED
+
+#include "aqsis.h"
+
+#include <vector>
+
+#include "channelinfo.h"
+#include "exception.h"
+
+namespace Aqsis {
+
+//------------------------------------------------------------------------------
+/** \brief Class holding an ordered list of image channels.
+ *
+ * This class describes the structure of a pixel in hetrogenous images (ie,
+ * images made up of possibly different types for each channel inside a pixel).
+ */
+class CqChannelList
+{
+	private:
+		/// The underlying container type holding the SqChannelInfo.
+		typedef std::vector<SqChannelInfo> TqListType;
+	public:
+		/// Iterator for channels
+		typedef TqListType::const_iterator const_iterator;
+
+		/// Construct an empty channel list.
+		inline CqChannelList();
+		/// Factory function for standard 8bpp RGB display channel list
+		static CqChannelList displayChannels();
+		/// We use the default copy constructor,destructor and assignment operator.
+
+		//----------------------------------------------------------------------
+		/// \name Standard iterator interface
+		//@{
+		/// Get an iterator to the start of the channel list.
+		inline const_iterator begin() const;
+		/// Get an iterator to the end of the channel list.
+		inline const_iterator end() const;
+		//@}
+
+		//----------------------------------------------------------------------
+		/// \name Vector interface
+		//@{
+		/// Add a channel to the end of the list.
+		void addChannel(const SqChannelInfo& newChan);
+		/// Get the number of channels in the list
+		inline TqInt numChannels() const;
+		/** \brief Get the channel at a given index.
+		 *
+		 * This function is range-checked.
+		 */
+		inline const SqChannelInfo& operator[](TqInt index) const;
+		//@}
+
+		//----------------------------------------------------------------------
+		/// \name Functions to search for channel names
+		//@{
+		/** \brief Get an index for the given channel name
+		 * \throw XqInternal if the channel name isn't in the list.
+		 * \return the index for the channel, if it exists.
+		 */
+		inline TqInt findChannelIndex(const std::string& name) const;
+		/// \brief Check whether the list of channels contains the given channel name
+		inline bool hasChannel(const std::string& name) const;
+		//@}
+
+		//----------------------------------------------------------------------
+		/// \name Access to pixel byte-layout
+		//@{
+		/** \brief Get the byte offset for the given channel number
+		 *
+		 * When channel data is packed contiguously in memory, the byte offset
+		 * is the number of bytes the indexed channel is away from the position
+		 * of the first channel inside a pixel.
+		 */
+		inline TqInt channelByteOffset(TqInt index) const;
+		/// Number of bytes required to store all channels in a pixel
+		inline TqInt bytesPerPixel() const;
+		//@}
+
+		/** \brief Reorder channels to the "expected" order (rgba)
+		 *
+		 * Reorder the list of channels to be in the standard order - "r", "g",
+		 * "b", "a".  Other channel names are ignored.
+		 */
+		void reorderChannels();
+
+	private:
+		/** \brief Get an index for the given channel name
+		 * \return the channel index, or -1 if not found.
+		 */
+		TqInt findChannelIndexImpl(const std::string& name) const;
+		/** \brief Recompute the cached channel byte offsets.
+		 */
+		void recomputeByteOffsets();
+
+		TqListType m_channels;  		///< underlying vector of SqChannelInfo
+		std::vector<TqInt> m_offsets;  ///< vector of byte offsets into the channels.
+		TqInt m_bytesPerPixel;			///< bytes per pixel needed to store the channels.
+};
+
+
+//==============================================================================
+// Implementation of inline functions and templates
+//==============================================================================
+inline CqChannelList::CqChannelList()
+	: m_channels(),
+	m_offsets(),
+	m_bytesPerPixel(0)
+{ }
+
+inline CqChannelList::const_iterator CqChannelList::begin() const
+{
+	return m_channels.begin();
+}
+
+inline CqChannelList::const_iterator CqChannelList::end() const
+{
+	return m_channels.end();
+}
+
+inline TqInt CqChannelList::numChannels() const
+{
+	return m_channels.size();
+}
+
+inline const SqChannelInfo& CqChannelList::operator[](TqInt index) const
+{
+	return m_channels.at(index);
+}
+
+inline TqInt CqChannelList::findChannelIndex(const std::string& name) const
+{
+	TqInt index = findChannelIndexImpl(name);
+	if(index < 0)
+		throw XqInternal("Cannot find channel with name \"" + name + "\"", __FILE__, __LINE__);
+	return static_cast<TqInt>(index);
+}
+
+inline bool CqChannelList::hasChannel(const std::string& name) const
+{
+	return findChannelIndexImpl(name) >= 0;
+}
+
+inline TqInt CqChannelList::channelByteOffset(TqInt index) const
+{
+	return m_offsets.at(index);
+}
+
+inline TqInt CqChannelList::bytesPerPixel() const
+{
+	return m_bytesPerPixel;
+}
+} // namespace Aqsis
+
+#endif // CHANNELLIST_H_INCLUDED
