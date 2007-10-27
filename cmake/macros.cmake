76e155467a26c4eff9727c2caa501861534a99b6
Chris Foster <c42f@users.sourceforge.net>
2007-10-27 14:44:49 +0000

    * Add cmake unit test macro from the texfiltering branch into the trunk.   Update tests to use the new macro.

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -34,8 +34,58 @@ MACRO(FILTER_OUT FILTERS INPUTS OUTPUT)
    SET(${OUTPUT} ${FOUT})
 ENDMACRO(FILTER_OUT FILTERS INPUTS OUTPUT)
 
+#----------------------------------------------------------------------
+# Macro to get the svn revision number
 MACRO( SVN_REPOSITORY_VERSION DESTVAR TOPDIR )
 	IF(NOT ${AQSIS_SVNVERSION_EXECUTABLE} STREQUAL "AQSIS_SVNVERSION_EXECUTABLE-NOTFOUND")
 		EXEC_PROGRAM( ${AQSIS_SVNVERSION_EXECUTABLE} ${TOPDIR} ARGS "." OUTPUT_VARIABLE ${DESTVAR} )
 	ENDIF(NOT ${AQSIS_SVNVERSION_EXECUTABLE} STREQUAL "AQSIS_SVNVERSION_EXECUTABLE-NOTFOUND")
 ENDMACRO ( SVN_REPOSITORY_VERSION )
+
+#----------------------------------------------------------------------
+# Facilities for adding unit tests to the build
+
+IF(AQSIS_ENABLE_TESTING)
+
+	# AQSIS_ADD_TESTS(SOURCE_LIST LINK_LIBS)
+	#
+	# Adds tests based on the boost.test framework.  Each source file is
+	# assumed to constitue an independent set of tests, and is built into its
+	# own executable.
+	#
+	# Arguments:
+	#   SOURCE_LIST - list of sources using boost.test.  One test set per file.
+	#   LINK_LIBS   - list of libraries which these tests need to be linked against.
+	#
+	# The boost test library is automatically linked with the generated
+	# executables, so there's no need to include it in the LINK_LIBS list.
+	#
+	MACRO(AQSIS_ADD_TESTS SOURCE_LIST LINK_LIBS)
+		FOREACH(TEST_SRC ${SOURCE_LIST})
+			# Remove any prefixed-path from the test source and remove the
+			# extension to get the test name.
+			GET_FILENAME_COMPONENT(TEST_EXE_NAME ${TEST_SRC} NAME_WE)
+			GET_FILENAME_COMPONENT(CURR_DIR_NAME ${PROJECT_SOURCE_DIR} NAME)
+			# Test name is "source_directory/file_name"
+			SET(TEST_NAME "${CURR_DIR_NAME}/${TEST_EXE_NAME}")
+			ADD_EXECUTABLE(${TEST_EXE_NAME}
+				${TEST_SRC}
+				${CMAKE_SOURCE_DIR}/build_tools/boostautotestmain.cpp
+				)
+			# Perhaps we should remove boostautotestmain.cpp in favor of simply
+			# defining BOOST_AUTO_TEST_MAIN in each test file.
+			TARGET_LINK_LIBRARIES(${TEST_EXE_NAME}
+				${LINK_LIBS}
+				${AQSIS_BOOST_UNIT_TEST_FRAMEWORK_LIBRARY}
+				)
+			ADD_TEST(${TEST_NAME} ${EXECUTABLE_OUTPUT_PATH}/${TEST_EXE_NAME})
+		ENDFOREACH(TEST_SRC)
+	ENDMACRO(AQSIS_ADD_TESTS)
+
+ELSE(AQSIS_ENABLE_TESTING)
+
+	# If testing is not enabled, AQSIS_ADD_TESTS is a No-op.
+	MACRO(AQSIS_ADD_TESTS SOURCE_LIST LINK_LIBS)
+	ENDMACRO(AQSIS_ADD_TESTS SOURCE_LIST LINK_LIBS)
+
+ENDIF(AQSIS_ENABLE_TESTING)
