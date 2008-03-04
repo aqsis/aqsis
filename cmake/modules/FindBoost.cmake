a8f989e30b5b3b170af06a6c33a2e21a6bcd730a
Leon Atkinson <ltatkinson@users.sourceforge.net>
2008-03-04 13:04:18 +0000

    Fixed typo in previous 'Boost Detection' commit and revised the library names (-mt) under non-Windows platforms.

--- cmake/modules/FindBoost.cmake
+++ cmake/modules/FindBoost.cmake
@@ -1,6 +1,6 @@
 SET(AQSIS_BOOST_FOUND 0)
 
-SET$(AQSIS_BOOST_VERSION_SUFFIX -1_34_1)
+SET(AQSIS_BOOST_VERSION_SUFFIX -1_34_1)
 
 SET(AQSIS_BOOST_LIBRARIES_DIR "" CACHE PATH "Location to look for boost libraries.")
 
@@ -24,10 +24,10 @@ IF(WIN32)
 		SET(AQSIS_BOOST_FILESYSTEM_LIBRARY_NAME libboost_filesystem-mgw34${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_filesystem library (undecorated, i.e. the string passed to the compiler)")
 	ENDIF(MSVC AND MSVC80)
 ELSE(WIN32)
-	SET(AQSIS_BOOST_THREAD_LIBRARY_NAME boost_thread${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_thread library (undecorated, i.e. the string passed to the compiler)")
-	SET(AQSIS_BOOST_UNIT_TEST_FRAMEWORK_LIBRARY_NAME boost_unit_test_framework${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_unit_test_framework library (undecorated, i.e. the string passed to the compiler)")
-	SET(AQSIS_BOOST_WAVE_LIBRARY_NAME boost_wave${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_wave library (undecorated, i.e. the string passed to the compiler)")
-	SET(AQSIS_BOOST_FILESYSTEM_LIBRARY_NAME boost_filesystem${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_filesystem library (undecorated, i.e. the string passed to the compiler)")
+	SET(AQSIS_BOOST_THREAD_LIBRARY_NAME boost_thread-mt${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_thread library (undecorated, i.e. the string passed to the compiler)")
+	SET(AQSIS_BOOST_UNIT_TEST_FRAMEWORK_LIBRARY_NAME boost_unit_test_framework-mt${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_unit_test_framework library (undecorated, i.e. the string passed to the compiler)")
+	SET(AQSIS_BOOST_WAVE_LIBRARY_NAME boost_wave-mt${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_wave library (undecorated, i.e. the string passed to the compiler)")
+	SET(AQSIS_BOOST_FILESYSTEM_LIBRARY_NAME boost_filesystem-mt${AQSIS_BOOST_VERSION_SUFFIX} CACHE STRING "The name of the boost_filesystem library (undecorated, i.e. the string passed to the compiler)")
 ENDIF(WIN32)
 MARK_AS_ADVANCED(AQSIS_BOOST_VERSION_SUFFIX)
 MARK_AS_ADVANCED(AQSIS_BOOST_THREAD_LIBRARY_NAME)
@@ -47,6 +47,7 @@ FIND_LIBRARY(AQSIS_BOOST_THREAD_LIBRARY
 			DOC "Location of the boost thread library"
 			)
 MARK_AS_ADVANCED(AQSIS_BOOST_THREAD_LIBRARY)
+
 FIND_LIBRARY(AQSIS_BOOST_UNIT_TEST_FRAMEWORK_LIBRARY
 			NAMES ${AQSIS_BOOST_UNIT_TEST_FRAMEWORK_LIBRARY_NAME}	
 			PATHS ${AQSIS_BOOST_LIBRARIES_DIR}
