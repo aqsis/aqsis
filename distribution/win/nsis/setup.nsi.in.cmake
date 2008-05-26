cf1f2258714c5ad024c3e94ec7efbdc0e0b616eb
Chris Foster <c42f@users.sourceforge.net>
2008-05-26 11:48:20 +0000

    Make sure the win32-specific headers for aqsistypes are installed. This oversight became apparent when fixing bug #1888255 since ri.h now depends on aqsis_compiler.h.

--- distribution/win/nsis/setup.nsi.in.cmake
+++ distribution/win/nsis/setup.nsi.in.cmake
@@ -199,6 +199,7 @@ Section /o "Libraries" SEC04
 SectionIn 1
   SetOutPath "$INSTDIR\include\aqsis"
   File "@CMAKE_SOURCE_DIR@\aqsistypes\*.h"
+  File "@CMAKE_SOURCE_DIR@\aqsistypes\win32\*.h"
   File "@CMAKE_SOURCE_DIR@\shadercompiler\shadervm\shadeop.h"
   File "@CMAKE_SOURCE_DIR@\rib\api\ri.h"
   File "@CMAKE_BINARY_DIR@\rib\api\ri.inl"
