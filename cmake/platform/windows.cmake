36a42a53110ff22ca6d89cb0a18a24835b0afb61
Chris Foster <c42f@users.sourceforge.net>
2008-07-06 15:10:29 +0000

    Try to fix an odd build failure on gentoo, as reported at http://bugs.gentoo.org/show_bug.cgi?id=230171
    We currently link to the dl library for dlopen(), dlclose() etc for each
    executable separately, but there doesn't seem to be a good reason for doing so.
    Instead I've chosen to link aqsistypes with the dl library; that dependency
    should then carry through to any executables which need it.  This was done by
    introducing a variable - AQSISTYPES_SYSTEM_LINKLIBS - and adding dl to it in
    the platform-specific cmake files.  Also I cleaned up a similar WIN32-specific
    library using the same variable.

--- cmake/platform/windows.cmake
+++ cmake/platform/windows.cmake
@@ -20,6 +20,8 @@ SET(SCRIPTSDIR "scripts"
 #   (b) Make sure that the math constants from math.h are defined - that is, M_PI etc.
 ADD_DEFINITIONS(-DNO_SYSLOG -D_USE_MATH_DEFINES)
 
+SET(AQSISTYPES_SYSTEM_LINKLIBS ${AQSISTYPES_SYSTEM_LINKLIBS} ws3_32)
+
 #
 # Create resource files for use during linking
 #
