58811e8cca4c4554c1964174b192bc52d9a2894f
Chris Foster <chris42f@gmail.com>
2009-05-19 16:16:28 +1000

    Work toward getting aqsis_core to compile with the new source tree
    This change renames some files in the new tree and cleans up the aqsis_core
    CMakeLists.txt.  Some XSLTs were also added to generate ri_cache.inl and
    friends.  The xsl_transform() cmake macro needed to be tweaked to correctly
    define dependencies when they're in another directory.
    
    As it turns out, there's some interface problems between aqsis_tex and
    aqsis_core which still need to be worked out before the core will compile, so
    the associated add_subdirectory() command is commented out for now.

--- newtree/extras/libs/core/api/project.cmake
+++ newtree/extras/libs/core/api/project.cmake
@@ -1,3 +1,16 @@
+# Generate API checking and object cache code etc using XSLT.
+
+set(_ri_cache_inl ${api_BINARY_DIR}/ri_cache.inl)
+set(_ri_debug_inl ${api_BINARY_DIR}/ri_debug.inl)
+set(_ri_validate_inl ${api_BINARY_DIR}/ri_validate.inl)
+
+xsl_transform(${_ri_cache_inl} ri.xml STYLESHEET ri_cache.xsl
+	SEARCHPATH "${api_SOURCE_DIR}:${ri_headers_SOURCE_DIR}")
+xsl_transform(${_ri_debug_inl} ri.xml STYLESHEET ri_debug.xsl
+	SEARCHPATH "${api_SOURCE_DIR}:${ri_headers_SOURCE_DIR}")
+xsl_transform(${_ri_validate_inl} ri.xml STYLESHEET ri_validate.xsl
+	SEARCHPATH "${api_SOURCE_DIR}:${ri_headers_SOURCE_DIR}")
+
 set(api_srcs
 	attributes.cpp
 	condition.cpp
@@ -19,10 +32,14 @@ set(api_hdrs
 	parameters.h
 	ri_cache.h
 	ri_debug.h
+	${_ri_cache_inl}
+	${_ri_debug_inl}
+	${_ri_validate_inl}
 )
 make_absolute(api_hdrs ${api_SOURCE_DIR})
 
 include_directories(${api_SOURCE_DIR})
+include_directories(${api_BINARY_DIR})
 
 set(api_test_srcs
 	rif_test.cpp
