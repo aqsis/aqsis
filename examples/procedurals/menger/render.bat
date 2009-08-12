994539ec30cafb7f05a7501895c302f5470cf941
Leon Atkinson <latkinson@aqsis1.osuosl.org>
2009-08-12 20:48:32 +0000

    Minor update to example content.

---
+++ examples/procedurals/menger/render.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+REM ***Render files***
+REM
+REM ECHO === Rendering File(s) ===
+REM ECHO.
+REM aqsis.exe -progress "menger.rib"
+REM IF ERRORLEVEL 0 GOTO end
+REM
+REM
+REM REM ***Error reporting***
+REM
+REM :error
+REM ECHO.
+REM ECHO.
+REM ECHO An error occured, please read messages !!!
+REM PAUSE
+REM EXIT
+REM :end
---
+++ examples/procedurals/menger/render.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ***Render files***
+
+echo "=== Rendering File(s) ==="
+echo
+aqsis -progress "menger.rib"
+
