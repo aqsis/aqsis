f32595980ae15064e90103603f5c0b68cbe81ec0
Chris Foster <chris42f@gmail.com>
2009-09-03 23:00:23 +1000

    Bugfix: Correct dependency handling for shader compiler build.
    The SOURCE argument to add_custom_command is deprecated in cmake-2.6, and
    doesn't add any dependencies to the custom command.  Dependencies on the yacc
    and lex sources are explicitly added by this patch.

--- libs/slcomp/parse/project.cmake
+++ libs/slcomp/parse/project.cmake
@@ -2,9 +2,9 @@
 set(_scanner_cpp_name ${parse_BINARY_DIR}/scanner.cpp)
 add_custom_command(
 	OUTPUT ${_scanner_cpp_name}
-	SOURCE ${parse_SOURCE_DIR}/scanner.ll
 	COMMAND ${AQSIS_FLEX_EXECUTABLE}
 		-o${_scanner_cpp_name} ${parse_SOURCE_DIR}/scanner.ll
+	MAIN_DEPENDENCY ${parse_SOURCE_DIR}/scanner.ll
 	DEPENDS ${parse_BINARY_DIR}/parser.hpp
 )
 
@@ -13,9 +13,9 @@ set(_parser_cpp_name ${parse_BINARY_DIR}/parser.cpp)
 set(_parser_hpp_name ${parse_BINARY_DIR}/parser.hpp)
 add_custom_command(
 	OUTPUT ${_parser_cpp_name} ${_parser_hpp_name}
-	SOURCE ${parse_SOURCE_DIR}/parser.yy
 	COMMAND ${AQSIS_BISON_EXECUTABLE}
 		-d -o ${_parser_cpp_name} ${parse_SOURCE_DIR}/parser.yy 
+	MAIN_DEPENDENCY ${parse_SOURCE_DIR}/parser.yy
 )
 
 # Create source list variables
