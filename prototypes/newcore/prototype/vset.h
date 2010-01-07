bfc7a5c7f82e2726a5274621f03d7ba80653b858
Chris Foster <chris42f@gmail.com>
2010-01-07 21:20:59 +1000

    vector-based set container.
    Perhaps this won't be needed after all, so it's going in the prototypes
    directory for now.

---
+++ prototypes/newcore/prototype/vset.h
@@ -0,0 +1,125 @@
+#ifndef VSET_H_INCLUDED
+#define VSET_H_INCLUDED
+
+#include <cassert>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+// forward declarations.
+template<typename T> class VSet;
+
+template<typename T1, typename T2, typename T3>
+void setUnion(const VSet<T1>& set1, const VSet<T2>& set2, VSet<T3>& result);
+template<typename T1, typename T2, typename T3>
+void setIntersection(const VSet<T1>& set1, const VSet<T2>& set2, VSet<T3>& result);
+
+
+/// Set class using a std::vector as the object container.
+///
+/// The idea of this set is that it should be more efficient in memory
+/// allocations and cache coherence than std::set for element containment
+/// testing and whole-set operations like union and intersection.  On the
+/// other hand, it's not designed to be modified in a per-element manner since
+/// doing so using the sorted std::vector implementation won't be efficient.
+///
+template<class T>
+class VSet
+{
+    private:
+        typedef std::vector<T> ContainerType;
+        ContainerType m_elems;
+
+    public:
+        typedef T value_type;
+        /// Iterator types
+        typedef typename ContainerType::iterator iterator;
+        typedef typename ContainerType::const_iterator const_iterator;
+
+        /// Create an empty set
+        VSet() : m_elems() { }
+
+        /// Initialize the set with the unordered sequence [b, e)
+        template<typename ForwardIterT>
+        VSet(ForwardIterT b, ForwardIterT e)
+            : m_elems()
+        {
+            assign(b, e);
+        }
+
+        /// Initialize the set with the unordered sequence [b, e)
+        template<typename ForwardIterT>
+        void assign(ForwardIterT b, ForwardIterT e)
+        {
+            m_elems.assign(b, e);
+            std::sort(m_elems.begin(), m_elems.end());
+        }
+
+        /// Iterator access to the contained, sorted, sequence
+        iterator begin() { return m_elems.begin(); }
+        iterator end() { return m_elems.end(); }
+        const_iterator begin() const { return m_elems.begin(); }
+        const_iterator end() const { return m_elems.end(); }
+
+        /// Random element access.
+        const value_type& operator[](int i) const
+        {
+            assert(i >= 0 && i < m_elems.size());
+            return m_elems[i];
+        }
+
+        /// Get the number of elements
+        int size() const { return m_elems.size(); }
+
+        /// Test whether elem is contained within the set
+        bool contains(const T& elem) const
+        {
+            return std::binary_search(m_elems.begin(), m_elems.end(), elem);
+        }
+
+        /// Test whether s is a subset of this set
+        bool includes(const VSet& s) const
+        {
+            return std::includes(m_elems.begin(), m_elems.end(),
+                                 s.begin(), s.end());
+        }
+
+        /// Set union between s1 & s2.
+        template<typename T1, typename T2, typename T3>
+        friend void setUnion(const VSet<T1>& set1, const VSet<T2>& set2,
+                             VSet<T3>& result);
+
+        /// Set intersection between s1 & s2.
+        template<typename T1, typename T2, typename T3>
+        friend void setIntersection(const VSet<T1>& set1, const VSet<T2>& set2,
+                                    VSet<T3>& result);
+};
+
+
+//==============================================================================
+
+template<typename T1, typename T2, typename T3>
+void setUnion(const VSet<T1>& set1, const VSet<T2>& set2,
+                        VSet<T3>& result)
+{
+    typename VSet<T3>::ContainerType& r = result.m_elems;
+    const typename VSet<T1>::ContainerType& s1 = set1.m_elems;
+    const typename VSet<T2>::ContainerType& s2 = set2.m_elems;
+    r.reserve(s1.size() + s2.size());
+    std::set_union(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                    std::back_inserter(r));
+}
+
+template<typename T1, typename T2, typename T3>
+void setIntersection(const VSet<T1>& set1, const VSet<T2>& set2,
+                            VSet<T3>& result)
+{
+    typename VSet<T3>::ContainerType& r = result.m_elems;
+    const typename VSet<T1>::ContainerType& s1 = set1.m_elems;
+    const typename VSet<T2>::ContainerType& s2 = set2.m_elems;
+    r.reserve(std::min(s1.size(), s2.size()));
+    std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                            std::back_inserter(r));
+}
+
+#endif // VSET_H_INCLUDED
---
+++ prototypes/newcore/prototype/vset_test.cpp
@@ -0,0 +1,91 @@
+#include "vset.h"
+
+#include <vector>
+#include <set>
+#include <cstdlib>
+#include <iostream>
+
+#define ARRAYEND(array) (array + sizeof(array)/sizeof(array[0]))
+
+__attribute__((noinline))
+int contains(const VSet<int>& s, int e)
+{
+    return s.contains(e);
+}
+
+__attribute__((noinline))
+int contains(const std::set<int>& s, int e)
+{
+    return s.find(e) != s.end();
+}
+
+void speedTest()
+{
+    const int timingIters = 10;
+    const int nsets = 1000000;
+    const int nelem = 10;
+    // Create some set data
+    std::vector<int> s1Init;
+    for(int i = 0; i < nelem; ++i)
+        s1Init.push_back(rand());
+    int* b = &s1Init[0];
+    int* e = &s1Init.back()+1;
+    // Create some sets; all the same actually.
+    typedef VSet<int> Set;
+//    typedef std::set<int> Set;
+    std::vector<Set> sets;
+    for(int j = 0; j < nsets; ++j)
+        sets.push_back(Set(b, e));
+    // Timing loop
+    long sum = 0;
+    for(long iter = 0; iter < timingIters; ++iter)
+    {
+        for(int j = 0; j < nsets; ++j)
+        {
+            for(int i = 0; i < nelem; ++i)
+                sum += contains(sets[j], s1Init[i]);
+        }
+    }
+    std::cout << sum << "\n";
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const VSet<T>& set)
+{
+    out << "{ ";
+    for(typename VSet<T>::const_iterator i = set.begin(), e = set.end(); i != e; ++i)
+        out << *i << " ";
+    out << "}";
+    return out;
+}
+
+void test()
+{
+    int s1Init[] = {1,6,7,3,99};
+    VSet<int> s1(s1Init, ARRAYEND(s1Init));
+    int s2Init[] = {1,-4,42,7,100};
+    VSet<int> s2(s2Init, ARRAYEND(s2Init));
+    std::cout << s1 << "\n";
+
+    std::cout << "s1.contains(1) = " << s1.contains(1) << "\n";
+    std::cout << "s1.contains(-1) = " << s1.contains(-1) << "\n";
+
+    int smallInit[] = {1,6};
+    VSet<int> small(smallInit, ARRAYEND(smallInit));
+    std::cout << s1 << ".includes(" << small << ") = " << s1.includes(small) << "\n";
+
+
+    VSet<int> u;
+    setUnion(s1, s2, u);
+    std::cout << "union(" << s1 << ", " << s2 << ") = " << u << "\n";
+    VSet<int> i;
+    setIntersection(s1, s2, i);
+    std::cout << "intersection(" << s1 << ", " << s2 << ") = " << i << "\n";
+}
+
+int main()
+{
+//    speedTest();
+    test();
+    return 0;
+};
