765b40a9199d58a715d387f8bb616a2e63757a43
Chris Foster <chris42f@gmail.com>
2010-06-20 23:08:04 +1000

    Experiments using OpenMP for parallelization
    This code implements multithreaded tiled fractal rendering with
    filtering using OpenMP to express parallelism.  The speedup is basically
    linear for 3 threads but is sublinear after that, possibly due to the
    lazy use of a critical section.

--- prototypes/newcore/experiments/fractal_threading.cpp
+++ prototypes/newcore/experiments/fractal_threading.cpp
@@ -5,7 +5,16 @@
 
 #include <tiffio.h>
 
+#include <boost/functional/hash.hpp>
 #include <boost/math/special_functions/sinc.hpp>
+#include <boost/scoped_array.hpp>
+#include <boost/thread.hpp>
+#include <boost/unordered_map.hpp>
+
+#include <OpenEXR/ImathVec.h>
+
+#include "../refcount.h"
+//#include "omp.h"
 
 // A define to disable inlining so that selected functions show up in the
 // profile.
@@ -47,7 +56,7 @@ void cacheFilter(std::vector<float>& filter, int filterWidth, int superSamp)
         {
             float x = (i+0.5)/superSamp - filterWidth/2.0;
             filter[j*fw + i] = windowedsinc(x, filterWidth) *
-                                        windowedsinc(y, filterWidth);
+                               windowedsinc(y, filterWidth);
         }
     }
     // normalize
@@ -120,7 +129,38 @@ void renderTile(float* rgb, int w, double x0, double y0, double dx, double dy,
 
 
 //------------------------------------------------------------------------------
-
+// Here's a diagram of how the filtering works.
+//
+//  A--------------------B--------------------+
+//  |                    |                    |
+//  |                    |                    |
+//  |                    |                    |
+//  |      .   .   .   . | .   .   .   .      |
+//  |                    |                    |
+//  |      .  P.....................Q  .      |
+//  |         .          |          .         |
+//  |      .  .          |          .  .      |
+//  |         .          |          .         |
+//  |      .  .          |          .  .      |
+//  |         .          |          .         |
+//  D---------.----------C----------.---------+
+//  |         .          |          .         |
+//  |      .  .          |          .  .      |
+//  |         .          |          .         |
+//  |      .  .          |          .  .      |
+//  |         .          |          .         |
+//  |      .  S.....................R  .      |
+//  |                    |                    |
+//  |      .   .   .   . | .   .   .   .      |
+//  |                    |                    |
+//  |                    |                    |
+//  |                    |                    |
+//  +--------------------+--------------------+
+//
+//  ----- ABCD is a sample tile
+//  ..... PQRS is a filter region
+//  .   . is the filter region expanded by the filter radius
+//
 noinline
 void filterAndQuantizeTile(uint8* rgbOut, const float* rgbTiles[2][2],
                            int tileWidth, int superSamp, const float* filter,
@@ -128,7 +168,7 @@ void filterAndQuantizeTile(uint8* rgbOut, const float* rgbTiles[2][2],
 {
     const int fw = filterWidth*superSamp;
     const int widthOn2 = tileWidth/2;
-    const int inTileWidth = superSamp*tileWidth; // Input tile size before filtering
+    const int sampTileWidth = superSamp*tileWidth; // Input tile size before filtering
     assert(tileWidth % 2 == 0);
     assert(filterWidth < widthOn2*superSamp);
     // Iterate over output tile
@@ -144,11 +184,11 @@ void filterAndQuantizeTile(uint8* rgbOut, const float* rgbTiles[2][2],
                     // location x,y of the current pixel in the 2x2 block
                     int x = superSamp*(ix + widthOn2) + i;
                     int y = superSamp*(iy + widthOn2) + j;
-                    int tx = x >= inTileWidth;
-                    int ty = y >= inTileWidth;
+                    int tx = x >= sampTileWidth;
+                    int ty = y >= sampTileWidth;
                     const float* c = rgbTiles[ty][tx] +
-                           3*( inTileWidth*(y - ty*inTileWidth) +
-                                           (x - tx*inTileWidth) );
+                           3*( sampTileWidth*(y - ty*sampTileWidth) +
+                                           (x - tx*sampTileWidth) );
                     float w = filter[fw*j + i];
                     col[0] += w*c[0];
                     col[1] += w*c[1];
@@ -158,23 +198,23 @@ void filterAndQuantizeTile(uint8* rgbOut, const float* rgbTiles[2][2],
 //            int xe = xb + fw;
 //            int yb = superSamp*(iy + widthOn2);
 //            int ye = yb + fw;
-//            if(    (xb < inTileWidth && xe >= inTileWidth)
-//                || (yb < inTileWidth && ye >= inTileWidth) )
+//            if(    (xb < sampTileWidth && xe >= sampTileWidth)
+//                || (yb < sampTileWidth && ye >= sampTileWidth) )
 //            {
 //                col[0] = 0;
 //            }
 //            else
 //            {
-//                const float* rgb = rgbTiles[yb >= inTileWidth][xb >= inTileWidth];
-//                if(xb >= inTileWidth)
-//                    xb -= inTileWidth;
-//                if(yb >= inTileWidth)
-//                    yb -= inTileWidth;
+//                const float* rgb = rgbTiles[yb >= sampTileWidth][xb >= sampTileWidth];
+//                if(xb >= sampTileWidth)
+//                    xb -= sampTileWidth;
+//                if(yb >= sampTileWidth)
+//                    yb -= sampTileWidth;
 //                for(int j = 0, y = yb; j < fw; ++j, ++y)
 //                {
 //                    for(int i = 0, x = xb; i < fw; ++i, ++x)
 //                    {
-//                        const float* c = rgb + 3*(inTileWidth*y + x);
+//                        const float* c = rgb + 3*(sampTileWidth*y + x);
 //                        float w = filter[fw*j + i];
 //                        col[0] += w*c[0];
 //                        col[1] += w*c[1];
@@ -217,7 +257,6 @@ void writeHeader(TIFF* tif, int width, int height, int tileWidth = -1)
 
 
 //------------------------------------------------------------------------------
-
 /// Simple sequential method.
 ///
 /// Render a big buffer of samples, then filter those samples.
@@ -260,6 +299,7 @@ void renderImageSimple(TIFF* tif, int width, int superSamp, const float* filter,
 }
 
 
+//------------------------------------------------------------------------------
 /// Tiled sequential method.
 ///
 /// Render samples in tiled chunks.  Filtering is interleaved (in time) with
@@ -279,8 +319,8 @@ void renderImageTiled(TIFF* tif, int width, int superSamp, const float* filter,
     writeHeader(tif, width, width, tileWidth);
     int ntiles = ceildiv(width, tileWidth) + 1;
 
-    int inTileWidth = superSamp*tileWidth;
-    int tileSize = inTileWidth*inTileWidth*3;
+    int sampTileWidth = superSamp*tileWidth;
+    int tileSize = sampTileWidth*sampTileWidth*3;
 
     // Storage pool for rendered tiles
     int poolSize = ntiles+2;
@@ -297,8 +337,8 @@ void renderImageTiled(TIFF* tif, int width, int superSamp, const float* filter,
         for(int tx = 0; tx < ntiles; ++tx, ++poolPos)
         {
             float* stor = tilePool[poolPos % poolSize];
-            renderTile(stor, inTileWidth, xoff + xmult*inTileWidth*tx,
-                       yoff + ymult*inTileWidth*ty, xmult, ymult, maxIter);
+            renderTile(stor, sampTileWidth, xoff + xmult*sampTileWidth*tx,
+                       yoff + ymult*sampTileWidth*ty, xmult, ymult, maxIter);
             if(ty > 0 && tx > 0)
             {
                 // When four adjacent tiles are rendered, filter the interior
@@ -317,12 +357,152 @@ void renderImageTiled(TIFF* tif, int width, int superSamp, const float* filter,
 }
 
 
+//------------------------------------------------------------------------------
+// Parallel tiled implementation.
+
+/// Tile containing sampled image color data
+struct SampTile : public RefCounted
+{
+    private:
+        int m_x;
+        int m_y;
+        boost::scoped_array<float> m_samps;
+    public:
+        SampTile(int x, int y, int width)
+            : m_x(x), m_y(y),
+            m_samps(new float[3*width*width])
+        { }
+
+        float* samps() { return m_samps.get(); }
+        const float* samps() const { return m_samps.get(); }
+};
+typedef boost::intrusive_ptr<SampTile> SampTilePtr;
+
+
+/// Holder for a 2x2 block of tiles waiting to be filtered
+struct TileFilterBlock
+{
+    SampTilePtr tiles[2][2];
+
+    bool readyForFilter() { return tiles[0][0] && tiles[0][1]
+                                && tiles[1][0] && tiles[1][1]; }
+};
+
+
+namespace Imath // namespace needed to allow ADL to find this :-(
+{
+/// Hash function for 2D points, copied from boost docs.
+std::size_t hash_value(Imath::V2i const& p)
+{
+    std::size_t seed = 0;
+    boost::hash_combine(seed, p.x);
+    boost::hash_combine(seed, p.y);
+    return seed;
+}
+}
+
+
+/// Parallel tiled method using OpenMP.
+noinline
+void renderImageParallelOmp(TIFF* tif, int width, int superSamp, const float* filter,
+                         int filterWidth, double xoff, double xmult, double yoff,
+                         double ymult, int maxIter)
+{
+    const int tileWidth = 16;
+    writeHeader(tif, width, width, tileWidth);
+    // number of filtered tiles.
+    int nftiles = ceildiv(width, tileWidth);
+    // number of sample tiles
+    int ntiles = nftiles + 1;
+
+    int sampTileWidth = superSamp*tileWidth;
+    int tileSize = sampTileWidth*sampTileWidth*3;
+
+    typedef boost::unordered_map<Imath::V2i, TileFilterBlock> FilterBlockContainer;
+    FilterBlockContainer waitingBlocks(2*nftiles);
+
+    // Render tiles left to right, top to bottom.
+    std::vector<uint8> colFilt(3*tileWidth*tileWidth, 0);
+#   pragma omp parallel
+    for(int ty = 0; ty < ntiles; ++ty)
+    {
+        // The options here are very important for correct load balancing,
+        // since the amount of work for a given tx,ty varies unpredictably.
+        //
+        // schedule(dynamic,1) tells the scheduler to give each thread
+        // _single_ values of tx (chunks of length 1) to work on at a time.
+        //
+        // nowait tells the scheduler that no thread synchronization is
+        // required at the end of the tx loop.
+#       pragma omp for schedule(dynamic,1) nowait
+        for(int tx = 0; tx < ntiles; ++tx)
+        {
+            SampTilePtr tile = new SampTile(tx, ty, sampTileWidth);
+            float* stor = tile->samps();
+            renderTile(stor, sampTileWidth, xoff + xmult*sampTileWidth*tx,
+                       yoff + ymult*sampTileWidth*ty, xmult, ymult, maxIter);
+            // The sample tile with coordinates (tx,ty) overlaps the
+            // four filtering tiles with coordinates:
+            //
+            //   (tx-1, ty-1)   (tx,ty-1)
+            //   (tx-1, ty)     (tx,ty)
+            //
+            // we insert the sample tile into each of these filtering tiles,
+            // ignoring filtering tile coordinates which lie outside the image
+            // boundaries.
+            for(int j = 0; j < 2; ++j)
+            for(int i = 0; i < 2; ++i)
+            {
+                Imath::V2i p(tx+i-1, ty+j-1);
+                // Ignore filtering tiles outside image boundaries
+                if(p.x >= 0 && p.y >= 0 && p.x < nftiles && p.y < nftiles)
+                {
+                    // using omp critical here is rather lazy, we could
+                    // parallelize the filtering too.
+#                   pragma omp critical
+                    {
+                        FilterBlockContainer::iterator blockIt = waitingBlocks.find(p);
+                        if(blockIt == waitingBlocks.end())
+                        {
+                            // Create blank block if it didn't exist
+                            std::pair<FilterBlockContainer::iterator, bool> insRes =
+                                waitingBlocks.insert(std::make_pair(p, TileFilterBlock()));
+                            assert(insRes.second);
+                            blockIt = insRes.first;
+                        }
+                        blockIt->second.tiles[1-j][1-i] = tile;
+                        if(blockIt->second.readyForFilter())
+                        {
+                            TileFilterBlock block = blockIt->second;
+                            waitingBlocks.erase(blockIt);
+                            // Filter, quantize & save result
+                            const float* toFilter[2][2] = {
+                                {block.tiles[0][0]->samps(), block.tiles[0][1]->samps()},
+                                {block.tiles[1][0]->samps(), block.tiles[1][1]->samps()},
+                            };
+                            filterAndQuantizeTile(&colFilt[0], toFilter, tileWidth,
+                                                  superSamp, &filter[0], filterWidth);
+                            TIFFWriteTile(tif, &colFilt[0], p.x*tileWidth,
+                                          p.y*tileWidth, 0, 0);
+                            // We're done with the block; remove it.
+                        }
+                    }
+                }
+            }
+        }
+    }
+    assert(waitingBlocks.size() == 0);
+}
+
+
+
+//------------------------------------------------------------------------------
 int main()
 {
     const int width = 800;
     const int maxIter = 2000;
 
-    const int superSamp = 2;
+    const int superSamp = 3;
 
     double scale = 0.01;
     double xmult = scale/superSamp * 2.0/width;
@@ -338,8 +518,10 @@ int main()
 
 //    renderImageSimple(tif, width, superSamp, &filter[0], filterWidth,
 //                      xoff, xmult, yoff, ymult, maxIter);
-    renderImageTiled(tif, width, superSamp, &filter[0], filterWidth,
-                     xoff, xmult, yoff, ymult, maxIter);
+//    renderImageTiled(tif, width, superSamp, &filter[0], filterWidth,
+//                     xoff, xmult, yoff, ymult, maxIter);
+    renderImageParallelOmp(tif, width, superSamp, &filter[0], filterWidth,
+                           xoff, xmult, yoff, ymult, maxIter);
 
     TIFFClose(tif);
 
