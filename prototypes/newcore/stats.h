42c9ae27529b6271df54e6190160b34f797237fc
Chris Foster <chris42f@gmail.com>
2010-09-22 20:53:15 +1000

    Quick & dirty debug tool for statistics reporting
    This patch adds a simple tool for quick & dirty reporting of statistics
    during debugging.

---
+++ prototypes/newcore/stats.h
@@ -0,0 +1,71 @@
+// Copyright (C) 2001, Paul C. Gregory and the other authors and contributors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the software's owners nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// (This is the New BSD license)
+
+#ifndef AQSIS_STATS_H_INCLUDED
+#define AQSIS_STATS_H_INCLUDED
+
+#include <iostream>
+#include <string>
+
+/// Class for quick and dirty statistics reporting.
+///
+/// Example to print the number of calls to a function foo() at the end of
+/// execution:
+///
+///   static SimpleStat<int> nFooCalls("number of foo() calls");
+///
+///   void foo(poly)
+///   {
+///       nFooCalls++;
+///   }
+///
+template<typename T>
+class SimpleStat
+{
+    public:
+        SimpleStat(const std::string& statName, const T& value = T())
+            : m_name(statName),
+            m_value(value)
+        { }
+
+        /// Implicit conversion to the value type.
+        operator T&() { return m_value; }
+
+        ~SimpleStat()
+        {
+            std::cout << m_name << " = " << m_value << std::endl;
+        }
+
+    private:
+        std::string m_name;
+        T m_value;
+};
+
+
+#endif // AQSIS_STATS_H_INCLUDED
