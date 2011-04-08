0f288615d662d893562705d3ba0282438f11d2b1
Chris Foster <chris42f@gmail.com>
2011-04-09 01:30:28 +1000

    Skeleton for point cloud viewer application
    The aim is to have a quick OpenGL preview of point clouds.  Not much
    is functional here yet, only an OpenGL window showing a set of axes
    along with mouse-based camera control.

---
+++ libs/pointrender/ptview.cpp
@@ -0,0 +1,186 @@
+// Copyright (C) 2001, Paul C. Gregory and the other authors and contributors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the software's owners nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// (This is the New BSD license)
+
+
+#include <QtGui/QApplication>
+
+#include "ptview.h"
+
+
+//------------------------------------------------------------------------------
+PointViewport::PointViewport(QWidget *parent)
+    : QGLWidget(parent),
+    m_prev_x(0),
+    m_prev_y(0),
+    m_zooming(false),
+    m_theta(0),
+    m_phi(0),
+    m_dist(5),
+    m_centre(0)
+{
+    setFocusPolicy(Qt::StrongFocus);
+}
+
+
+void PointViewport::initializeGL()
+{
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_POINT_SMOOTH);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+
+    // background colour
+    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+
+    // set up Z buffer
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+}
+
+
+void PointViewport::resizeGL(int w, int h)
+{
+    // Draw on full window
+    glViewport(0, 0, w, h);
+}
+
+
+void PointViewport::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Set camera projection
+    glMatrixMode(GL_PROJECTION);
+        glLoadIdentity( );
+        const float n = 0.1f;
+        const float hOn2 = 0.5f*n;
+        const float wOn2 = hOn2*width()/height();
+        glFrustum(-wOn2, wOn2, -hOn2, hOn2, n, 1000);
+        glScalef(1, 1, -1);
+        glTranslatef(0, 0, m_dist);
+        glRotatef(m_theta, 1, 0, 0);
+        glRotatef(m_phi, 0, 1, 0);
+        glTranslatef(m_centre.x, m_centre.y, m_centre.z);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Draw axes
+    glColor3f(1.0,0.0,1.0);
+    glEnable(GL_LINE_SMOOTH);
+    glLineWidth(1);
+    glColor3f(1.0, 0.7, 0.7);
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(1, 0, 0);
+    glEnd();
+    glColor3f(0.7, 1.0, 0.7);
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 1, 0);
+    glEnd();
+    glColor3f(0.7, 0.7, 1.0);
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 0, 1);
+    glEnd();
+
+    // Draw points
+    // TODO
+    glPointSize(10);
+    glColor3f(1,1,1);
+    glBegin(GL_POINTS);
+        glVertex3f(0,0,0);
+    glEnd();
+}
+
+
+void PointViewport::mousePressEvent(QMouseEvent* event)
+{
+    m_zooming = event->button() == Qt::RightButton;
+    m_prev_x = event->x();
+    m_prev_y = event->y();
+}
+
+
+void PointViewport::mouseMoveEvent(QMouseEvent* event)
+{
+    int dx = m_prev_x - event->x();
+    int dy = m_prev_y - event->y();
+    m_prev_x = event->x();
+    m_prev_y = event->y();
+    if(m_zooming)
+    {
+        m_dist *= std::pow(0.9, 30*double(dy)/height());
+    }
+    else
+    {
+        if(event->modifiers() & Qt::ControlModifier)
+        {
+            m_centre.y +=  m_dist/height()*dy;
+            m_centre.x += -m_dist/width()*dx*std::cos(deg2rad(m_phi));
+            m_centre.z += -m_dist/width()*dx*std::sin(deg2rad(m_phi));
+        }
+        else
+        {
+            m_theta += 0.5*dy;
+            m_phi   += 0.5*dx;
+        }
+    }
+    repaint();
+}
+
+
+void PointViewport::wheelEvent(QWheelEvent* event)
+{
+    double degrees = event->delta()/8.0;
+    double steps = degrees / 15.0;
+    m_dist *= std::pow(0.9, steps);
+    repaint();
+}
+
+
+void PointViewport::keyPressEvent(QKeyEvent *event)
+{
+    event->ignore();
+}
+
+
+//------------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    PointViewerWindow window;
+    window.show();
+
+    return app.exec();
+}
+
+// vi: set et:
---
+++ libs/pointrender/ptview.h
@@ -0,0 +1,109 @@
+// Copyright (C) 2001, Paul C. Gregory and the other authors and contributors
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the software's owners nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// (This is the New BSD license)
+
+#ifndef AQSIS_PTVIEW_H_INCLUDED
+#define AQSIS_PTVIEW_H_INCLUDED
+
+#include <cmath>
+
+#include <QtGui/QMainWindow>
+#include <QtGui/QKeyEvent>
+#include <QtOpenGL/QGLWidget>
+
+#include <OpenEXR/ImathVec.h>
+
+using Imath::V3f;
+
+inline float deg2rad(float d) { return (M_PI/180) * d; }
+
+
+//------------------------------------------------------------------------------
+/// OpenGL-based viewer widget for point clouds.
+class PointViewport : public QGLWidget
+{
+    Q_OBJECT
+
+    public:
+        PointViewport(QWidget *parent = NULL);
+
+    protected:
+        // Qt OpenGL callbacks
+        void initializeGL();
+        void resizeGL(int w, int h);
+        void paintGL();
+
+        // Qt event callbacks
+        void mousePressEvent(QMouseEvent* event);
+        void mouseMoveEvent(QMouseEvent* event);
+        void wheelEvent(QWheelEvent* event);
+        void keyPressEvent(QKeyEvent *event);
+
+    private:
+        // Mouse-based view positioning
+        int m_prev_x;
+        int m_prev_y;
+        bool m_zooming;
+        float m_theta;
+        float m_phi;
+        float m_dist;
+        V3f m_centre;
+};
+
+
+//------------------------------------------------------------------------------
+/// Main window for point cloud viewer application
+class PointViewerWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    public:
+        PointViewerWindow()
+        {
+            m_pointViewport = new PointViewport();
+            m_pointViewport->setMinimumSize(QSize(640, 480));
+            setCentralWidget(m_pointViewport);
+            setWindowTitle("Aqsis point cloud viewer");
+        }
+
+    protected:
+        void keyReleaseEvent(QKeyEvent* event)
+        {
+            if(event->key() == Qt::Key_Escape)
+                close();
+        }
+
+    private:
+        PointViewport* m_pointViewport;
+};
+
+
+
+#endif // AQSIS_PTVIEW_H_INCLUDED
+
+// vi: set et:
