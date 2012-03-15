ffe3816d297ce0b31df08a4a6aba237c88dbc07d
Chris Foster <chris42f@gmail.com>
2012-03-15 22:48:24 +1000

    Bugfix #3505124: RiDeformationV declared but not defined
    Small fix from Pietro Cerutti: RiDeformationV is declared in
    include/aqsis/ri/ri.h but never defined, this causes linking issues with
    software that invokes it (in particular ayam).

--- libs/core/api/ri.cpp
+++ libs/core/api/ri.cpp
@@ -4220,6 +4220,11 @@ RtVoid RiDeformation(RtToken name, PARAMETERLIST)
 	Aqsis::log() << warning << "RiDeformation not supported" << std::endl;
 }
 
+RtVoid RiDeformationV(RtToken name, RtInt n, RtToken tokens[], RtPointer parms[])
+{
+	Aqsis::log() << warning << "RiDeformationV not supported" << std::endl;
+}
+
 RtVoid RiMakeBump(RtString imagefile, RtString bumpfile, RtToken swrap, RtToken twrap, RtFilterFunc filterfunc, RtFloat swidth, RtFloat twidth, PARAMETERLIST)
 {
 	Aqsis::log() << warning << "RiMakeBump not supported" << std::endl;
